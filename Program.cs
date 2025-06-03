4e5a62d9a4b94381a85dd6de543fecfff9ba9da2
Sneaky365 <ivandimitrov0705@gmail.com>
2025-06-03 22:21:21 +0300

    Main Page Text done

--- Program.cs
+++ Program.cs
@@ -6,5 +6,21 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        static void MainPage()
+        {
+            string text =
+                        @" ------  ---------------   --------   -----------------    ----------------
+                          | Menu | Reserved Tables | Receipts | Restaurant Orders? | TakeAway Orders  | 
+                           ------  ---------------   --------   -----------------    ----------------
+                             ||           ||            ||             ||                   ||
+                             \/           \/            \/             \/                   \/
+                             #1           #2            #3             #4                   #5
+
+                                                                                               
+                             >>> Return Home <<< (Press R)
+                            ";
+            
+            
+        }
     }
 }
