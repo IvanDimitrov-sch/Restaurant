7700701df9c8218772e9049cf384c1e24da527eb
Sneaky365 <ivandimitrov0705@gmail.com>
2025-06-03 23:11:30 +0300

    DishesNode starting

---
+++ DishesNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant;
+internal class DishesNode
+{
+    public double Price { get; set; }
+    public string Name { get; set; }
+    public int AbsPrice { get; set; }
+
+    public DishesNode Left;
+    public DishesNode Right;
+    public int Height;
+
+    DishesNode(string name, double price)
+    {
+        Price = price;
+        Name = name;
+        AbsPrice = (int)Math.Floor(Price);
+    }
+}
